a8d9195510a42210ee3d671053be8b0c1b904935
Doniyorbek Mashrabboev <doniyorbek.mashrabboyev@gmail.com>
2020-12-11 13:23:36 +0500

    Initial Commit

---
+++ SqlIteTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SqlIteTest
+//
+//  Created by Doniyorbek on 12/11/20.
+//  Copyright © 2020 Bekmurod Turgunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
